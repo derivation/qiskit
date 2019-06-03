005e276585d9cf1104d85998cf8309242ed00d08
SooluThomas <soolu.elto@gmail.com>
2019-06-03 10:33:48 -0400

    Added compile_documentation.sh

--- tools/complie_documentation.sh
+++ tools/complie_documentation.sh
@@ -15,28 +15,26 @@
 # Script for pushing the documentation to the qiskit.org repository.
 
 # Non-travis variables used by this script.
-TARGET_REPOSITORY="git@github.com:SooluThomas/testTranslation.git"
-TARGET_DOC_DIR="."
-SOURCE_DOC_DIR="docs/_build/html"
+TARGET_REPOSITORY="git@github.com:SooluThomas/qiskit.git"
+TARGET_BRANCH="translationDocs"
+DOC_DIR_1="docs/_build/gettext"
+DOC_DIR_2="docs/locale"
 SOURCE_DIR=`pwd`
 TRANSLATION_LANG='ja'
 
-function doCompile {
-  ./compile_documentation.sh
-}
+echo "cd docs"
+cd docs
+# Extract document's translatable messages into pot files
+echo "Extract document's translatable messages into pot files: "
+sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext/$TRANSLATION_LANG
 
-# Build the documentation.
-echo "make doc"
-make doc
-echo "end of make doc"
-
-#Run the compile script
-doCompile
+# Setup / Update po files
+echo "Setup / Update po files"
+sphinx-intl update -p _build/gettext -l $TRANSLATION_LANG
 
 # Setup the deploy key.
 # https://gist.github.com/qoomon/c57b0dc866221d91704ffef25d41adcf
 echo "set ssh"
-pwd
 set -e
 openssl aes-256-cbc -K $encrypted_a301093015c6_key -iv $encrypted_a301093015c6_iv -in ../tools/github_deploy_key.enc -out github_deploy_key -d
 chmod 600 github_deploy_key
@@ -46,36 +44,26 @@ echo "end of configuring ssh"
 
 # Clone the landing page repository.
 cd ..
-pwd
-echo "git clone"
+echo "git checkout and git clone"
+git checkout $TARGET_BRANCH
 git clone --depth 1 $TARGET_REPOSITORY tmp
 cd tmp
 git config user.name "SooluThomas"
 git config user.email "soolu.elto@gmail.com"
 
-# Selectively delete files from the dir, for preserving versions and languages.
-echo "git rm -rf"
-git rm -rf --ignore-unmatch $TARGET_DOC_DIR/*.html \
-    $TARGET_DOC_DIR/_* \
-    $TARGET_DOC_DIR/aer \
-    $TARGET_DOC_DIR/autodoc \
-    $TARGET_DOC_DIR/aqua \
-    $TARGET_DOC_DIR/terra \
-    $TARGET_DOC_DIR/ignis
-
-
 # Copy the new rendered files and add them to the commit.
 # mkdir -p $TARGET_DOC_DIR
 echo "copy directory"
-cp -r $SOURCE_DIR/$SOURCE_DOC_DIR/* $TARGET_DOC_DIR/
+cp -r $SOURCE_DIR/$DOC_DIR_1/* $DOC_DIR_1/
+cp -r $SOURCE_DIR/$DOC_DIR_2/* $DOC_DIR_2/
 
 # git checkout translationDocs
-echo "add to target dir"
-git add $TARGET_DOC_DIR
+echo "add to pot files to target dir"
+git add $DOC_DIR_1
+git add $DOC_DIR_2
 
 # Commit and push the changes.
 echo "git commit"
-git commit -m "Automated documentation update from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+git commit -m "Automated documentation update to add .po and .pot files from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
 echo "git push"
-git push --quiet
-# origin master
+git push --quiet origin $TARGET_BRANCH
