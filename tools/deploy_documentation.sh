4bde98d6c94021dabc4aef0e3e61c4aaea11c4de
SooluThomas <soolu.elto@gmail.com>
2019-06-04 11:21:56 -0400

    Updated bash file

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -17,33 +17,12 @@
 # Non-travis variables used by this script.
 TARGET_REPOSITORY="git@github.com:SooluThomas/testTranslation.git"
 TARGET_DOC_DIR="."
-SOURCE_DOC_DIR="docs/_build/html"
+SOURCE_DOC_DIR="docs/_build/html/locale"
 SOURCE_DIR=`pwd`
 TRANSLATION_LANG='ja'
 
-# SOURCE_REPOSITORY="git@github.com:SooluThomas/qiskit.git"
-# SOURCH_BRANCH="translationDocs"
-DOC_DIR_1="docs/_build/gettext"
-DOC_DIR_2="docs/locale"
-
-# Build the documentation.
-# echo "make doc"
-# make doc
-# echo "end of make doc"
-
-# echo "show current dir: "
-# pwd
-
 echo "cd docs"
 cd docs
-# Extract document's translatable messages into pot files
-# https://sphinx-intl.readthedocs.io/en/master/quickstart.html
-# echo "Extract document's translatable messages into pot files: "
-# sphinx-build -b gettext -D language=$TRANSLATION_LANG . _build/gettext/$TRANSLATION_LANG
-
-# Setup / Update po files
-# echo "Setup / Update po files"
-# sphinx-intl update -p _build/gettext -l $TRANSLATION_LANG
 
 # Make translated document
 # make -e SPHINXOPTS="-Dlanguage='ja'" html
@@ -61,33 +40,6 @@ eval $(ssh-agent -s)
 ssh-add github_deploy_key
 echo "end of configuring ssh"
 
-# Clone to the working repository for .po and pot files
-# cd ..
-# pwd
-# echo "git clone for working repo"
-# git clone --depth 1 $SOURCE_REPOSITORY temp --single-branch --branch $SOURCH_BRANCH
-# cd temp
-# git branch
-# git config user.name "SooluThomas"
-# git config user.email "soolu.elto@gmail.com"
-
-# Copy the new rendered files and add them to the commit.
-# echo "copy directory"
-# cp -r $SOURCE_DIR/$DOC_DIR_1/ $DOC_DIR_1/
-# cp -r $SOURCE_DIR/$DOC_DIR_2/ $DOC_DIR_2/
-
-# git checkout translationDocs
-# echo "add to pot files to target dir"
-# git add -f $DOC_DIR_1
-# git add $DOC_DIR_2
-
-# Commit and push the changes.
-# echo "git commit"
-# git commit -m "Automated documentation update to add .po and .pot files from meta-qiskit" -m "Commit: $TRAVIS_COMMIT" -m "Travis build: https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-# echo "git push"
-# git push --quiet origin $TARGET_BRANCH
-# echo "********** End of pushing po and pot files to working repo! *************"
-
 # Clone the landing page repository.
 cd ..
 pwd
@@ -97,7 +49,7 @@ cd tmp
 git config user.name "SooluThomas"
 git config user.email "soolu.elto@gmail.com"
 
-# Selectively delete files from the dir, for preserving versions and languages.
+Selectively delete files from the dir, for preserving versions and languages.
 echo "git rm -rf"
 git rm -rf --ignore-unmatch $TARGET_DOC_DIR/*.html \
     $TARGET_DOC_DIR/_* \
